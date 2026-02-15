e2bd8a92264892c091ca3e2d7185b5f378242672
Polarking8 <57197175+Polarking8@users.noreply.github.com>
2026-02-15 11:56:02 -0700

    Pintable mega4.3
    Added pin table, removed previous unneeded code

--- Mega4.3/Mega4.3.ino
+++ Mega4.3/Mega4.3.ino
@@ -6,13 +6,56 @@ PWMServo Servo;
 // Variable Intialization
 unsigned long time = 0;
 unsigned long time_old = 0;
-
-
-int receivedLEDValue = 0;
-int receivedServoAngle  = 0;
+// int receivedLEDValue = 0;
+// int receivedServoAngle  = 0;
 //Pin table
-int LEDpin = 13;
-int ServoPin = 11;
+//________________ Serial comms
+int USBRXCable = 0;
+int USBTXCable = 1;
+//________________ Dual Motor Shield
+int M1ENDual = 2;
+int DriveEncoder1ADual = 3;
+int DriveEncoder1BDual = 18;
+int M1DIAGDual = 6; 
+int M1DIRDual = 7;
+int M1PWMDual = 9;
+int M1OCMDual = A0; 
+int M2ENDual = 4;
+int M2DIRDual = 8;
+int M2PWMDual = 10; 
+int M2DIAGDual = 12;
+int DriveEncoder2ADual = 19;
+int DriveEncoder2BDual = 20;
+int M2OCMDual = A1;
+//_________________ Xbee Shield
+int XBeeTX = 16;
+int XBeeRX = 17; 
+//_________________ Pusher servo pin
+int ButtonServoPWM0 = 11;
+//_________________ Solo Motor Driver Shield
+int M1DIAGsolo = 22;
+int M1PWMsolo = 44;
+int M2PWMsolo = 45;
+int M1OCMsolo = A1;
+//_________________ Reflectance Array
+int Reflect1 = 25;
+int Reflect2 = 26;
+int Reflect3 = 27;
+int Reflect4 = 28;
+int Reflect5 = 29;
+int Reflect6 = 30;
+int Reflect7 = 31;
+//_________________ Color Sensor
+int Color1 = 32;
+int Color2 = 33;
+int Color3 = 34;
+int Color4 = 35;
+int Color5 = 36;
+int Color6 = 37;
+//_________________ Hall Effect 
+int HallEffect = A3;
+//_________________ Distance Sensor
+int DistanceSensor = A4;
 
 //End Pin table
 void setup()  
@@ -26,28 +69,13 @@ void setup()
   // for wireless comms, it also needs to match the Xbee firmware setting of 115200
   // Send a message to the other Arduino board
   Serial1.print("Hello other Arduino!");
-  pinMode(LEDpin,OUTPUT);
-  Servo.attach(ServoPin);
+  //pinMode(LEDpin,OUTPUT);
+  //Servo.attach(ServoPin);
 }
-
-//void loop() // run over and over
-//{
-//  time = millis();
-//
-//  if(time-time_old >= 2000) {
-//    Serial.print("Hello World - ");
-//    Serial.print(float(time)/1000);
-//    Serial.println(" sec");
-//    time_old = time;
-//  }
-//
-//}
-
 void loop(){
   // if (Serial.available()) {
   //   Serial1.println(Serial.readStringUntil('\n'));
   // }
-
   // if (Serial1.available()) {
   //   Serial.println(Serial1.readStringUntil('\n'));
   // }
@@ -57,8 +85,7 @@ void loop(){
       receivedServoAngle = Serial1.read();
     }
   }
-  digitalWrite(LEDpin,receivedLEDValue);
-  Serial.println(receivedServoAngle);
-
-  Servo.write(receivedServoAngle);
+  //digitalWrite(LEDpin,receivedLEDValue);
+  //Serial.println(receivedServoAngle);
+  //Servo.write(receivedServoAngle);
 }
