dcf63ad945f770d19c25b69981f2fc7c7b34318b
Polarking8 <57197175+Polarking8@users.noreply.github.com>
2026-04-02 00:18:16 -0600

    line following code? TODO: figure out velocity calculation

--- MegaComp4.3/MegaComp4.3.ino
+++ MegaComp4.3/MegaComp4.3.ino
@@ -198,6 +198,7 @@ double lineAid = 0; //sensor readings weighted for distance
 bool onLine = false; //true if is over the line and reading is valid
 double linePosition = 0; //where the line is relative to the center of the sensor in cm
 double linePositionDes = 2.8;//2.8 = center of sensor
+double lineError = 0;
 
 //Color Sensor Vars
 const int colorNumSamples = 8;
@@ -352,6 +353,27 @@ void loop(){
       //sets mLVelDes, and mLVelDes
       if (followingLine){
         //put code to find mLVelDes and mRVelDes based on line position.
+        qtr.read(sensor_Values);
+        for (int i = 0; i < SensorCount; i++){
+          lineSensorValuesUnbiased[i] = lineSensorValues[i] - lineSensorBias[i]; 
+          if(lineSensorValuesUnbiased[i]>5000){// Read all line sensor values, reset any that are too high (prevent overflow)
+            lineSensorValuesUnbiased[i] = 0;
+          }
+        // Serial2.print(lineSensorValuesUnbiased[i]); 
+        // Serial2.print('\t');
+        lineAi = 0; //Number calculation for finding center
+        lineAid = 0;
+        for(int i = 0; i < SensorCount; i++) { 
+          lineAid = lineAid + lineSensorValuesUnbiased[i] * lineSensorPositions[i]; // calculate lineAid
+          lineAi = lineAi + lineSensorValuesUnbiased[i]; // Calculate LineAi
+        }
+        linePosition = lineAid/lineAi; // Find where on the sensor we are at
+        lineError = linePositionDes- double(linePosition); //find how far from zero that is
+
+        //TODO : Find a way to turn this motor val calculations into mRVelDes and mLVelDes
+        //RightMotorVal = base_speed + Kp*lineError;
+        //LeftMotorVal = base_speed - Kp*lineError;
+      }
       } else{
         //calulate velocities with ramsete only if not line following
         Ramsete();
