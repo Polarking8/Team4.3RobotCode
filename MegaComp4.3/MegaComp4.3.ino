25ef72f1e61e3e7c2893a1801d981a67c21fb089
nicholas-jacob <50890681+nicholas-jacob@users.noreply.github.com>
2026-04-17 18:01:54 -0600

    Updated the main section with button pushing
    (270ms button pushing is much worse)

--- MegaComp4.3/MegaComp4.3.ino
+++ MegaComp4.3/MegaComp4.3.ino
@@ -365,13 +365,13 @@ void loop(){
           digitalWrite(SecondConveyorPin, HIGH);
 
           //start spaming button
-          if (((timeMS-timeMSpusher_old)>175) && (isPushed)) { 
+          if (((timeMS-timeMSpusher_old)>165) && (isPushed)) { 
             timeMSpusher_old = timeMS;
             servoAngle = servoRetractPos;
             servo2Angle = servo2RetractPos;
             isPushed = false;
           }
-          if (((timeMS-timeMSpusher_old)>125) && (!isPushed)) { 
+          if (((timeMS-timeMSpusher_old)>115) && (!isPushed)) { 
             timeMSpusher_old = timeMS;
             servoAngle = servoPushPos;
             servo2Angle = servo2PushPos;
@@ -600,13 +600,13 @@ void loop(){
           conveyorPower = -400;
 
           //start spaming button
-          if (((timeMS-timeMSpusher_old)>125) && (isPushed)) { 
+          if (((timeMS-timeMSpusher_old)>165) && (isPushed)) { 
             timeMSpusher_old = timeMS;
             servoAngle = servoRetractPos;
             servo2Angle = servo2RetractPos;
             isPushed = false;
           }
-          if (((timeMS-timeMSpusher_old)>125) && (!isPushed)) { 
+          if (((timeMS-timeMSpusher_old)>115) && (!isPushed)) { 
             timeMSpusher_old = timeMS;
             servoAngle = servoPushPos;
             servo2Angle = servo2PushPos;
